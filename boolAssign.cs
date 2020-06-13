f3569d295a2fdef056b89b4e0025b9537423b2c8
Chromatonics <61987650+Chromatonics@users.noreply.github.com>
2020-06-13 13:50:18 -0600

    Add files via upload

---
+++ boolAssign.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace boolean1.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            int yAge = Convert.ToInt32(yourAge);
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string haveDUI = Console.ReadLine();
+            bool hDUI = Convert.ToBoolean(haveDUI);
+
+            Console.WriteLine("How many Speeding Tickets?");
+            string speedTick = Console.ReadLine();
+            int sTick = Convert.ToInt32(speedTick);
+
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (yAge >= 15 && hDUI == false && sTick <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+        }
+    }
+}
