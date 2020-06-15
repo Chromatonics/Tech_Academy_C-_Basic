8fbc56a7acdb1530121e3ab6c781eb987e9d634a
Chromatonics <61987650+Chromatonics@users.noreply.github.com>
2020-06-15 16:21:05 -0600

    Add files via upload

---
+++ boolComparison/boolComparison/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess word");
+            string word = Console.ReadLine();
+            bool isGuessed = word == "cat";
+
+            do
+            {
+                switch (word)
+                {
+                    case "cat":
+                        Console.WriteLine("you guessed cat. you are correct");
+                        isGuessed = true;
+                        break;
+                    case "dog":
+                        Console.WriteLine("you guessed dog. try again");
+                        Console.WriteLine("guess a word");
+                        word = Console.ReadLine();
+                        break;
+                    case "ferret":
+                        Console.WriteLine("you guessed ferret. try again");
+                        Console.WriteLine("guess a word");
+                        word = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("wrong");
+                        Console.WriteLine("guess a word");
+                        word = Console.ReadLine();
+                        break;
+
+                }
+            }
+            while (!isGuessed);
+    
+
+            Console.ReadLine();
+        }
+    }
+}
+
