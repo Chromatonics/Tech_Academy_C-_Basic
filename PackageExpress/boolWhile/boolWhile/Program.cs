9f108db5c0265748099146675fb3e767326c5b1f
Chromatonics <61987650+Chromatonics@users.noreply.github.com>
2020-06-15 16:19:22 -0600

    Add files via upload

---
+++ PackageExpress/boolWhile/boolWhile/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess letter");
+            string letter = Console.ReadLine();
+            bool isGuessed = letter == "a";
+
+            while (!isGuessed)
+            {
+                switch (letter)
+                {
+                    case "a":
+                        Console.WriteLine("you guessed a. you are correct");
+                        isGuessed = true;
+                        break;
+                    case "b":
+                        Console.WriteLine("you guessed b. try again");
+                        Console.WriteLine("guess a word");
+                        letter = Console.ReadLine();
+                        break;
+                    case "c":
+                        Console.WriteLine("you guessed c. try again");
+                        Console.WriteLine("guess a word");
+                        letter = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("wrong");
+                        Console.WriteLine("guess a letter");
+                        letter = Console.ReadLine();
+                        break;
+
+                }
+            }
+            Console.ReadLine();
+        }
+            
+        }
+    }
+
