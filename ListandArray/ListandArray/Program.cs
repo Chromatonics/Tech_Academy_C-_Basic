18da9133f078b5a620c75a27671c227e3a1a9378
Chromatonics <61987650+Chromatonics@users.noreply.github.com>
2020-06-16 13:35:36 -0600

    Add files via upload

---
+++ ListandArray/ListandArray/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        List<string> sList = new List<string>();
+        sList.Add("Hello");
+        sList.Add("World");
+        sList.Remove("It's me!");
+
+        Console.WriteLine("Select an Index:");
+        string selection = Console.ReadLine();
+        int sSelect = Convert.ToInt32(selection);
+        Console.WriteLine(sList[sSelect]);
+        Console.ReadLine();
+
+
+        string[] strArray = new string[] { "Hi", "there", "this", "that" };
+
+        Console.WriteLine("Select an Index:");
+        string sArray1 = Console.ReadLine();
+        int sArray2 = Convert.ToInt32(sArray1);
+       
+        if (sArray2 > 3)
+        {
+            Console.WriteLine("This index does not exist");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine(strArray[sArray2]);
+            Console.ReadLine();
+        }
+
+
+        int[] numArray1 = new int[] { 5, 2, 10, 12 };
+
+        Console.WriteLine("Select an Index:");
+        string index1 = Console.ReadLine();
+        int index2 = Convert.ToInt32(index1);
+
+        if (index2 > 3)
+        {
+            Console.WriteLine("This index does not exist");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine(numArray1[index2]);
+            Console.ReadLine();
+        }
+    }
+}
