2200aad085ad51b5604a470fbd9851d97b948088
Chromatonics <Pallaslih6@gmail.com>
2020-07-23 15:47:15 -0600

    Method Assignment

---
+++ MethodAssign/MethodAssign/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssign
+{
+    class math
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Picka number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 2;
+            int r;
+            int s;
+            int t;
+            calculate n = new calculate();
+
+            r = n.divideNum(a, b);
+            Console.WriteLine( a + " divided by " + b + " equals " + r);
+            Console.ReadLine();
+
+            s = n.multiplyNum(a, b);
+            Console.WriteLine(a + " multiplied by " + b + " equals " + s);
+            Console.ReadLine();
+
+            t = n.addNum(a, b);
+            Console.WriteLine(a + " added to " + b + " equals " + t);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodAssign/MethodAssign/math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssign
+{
+    public class calculate
+    {
+        public int divideNum(int num1, int num2)
+        {
+            int result;
+            result = num1 / num2;
+            return result;
+        }
+
+        public int multiplyNum(int num1, int num2)
+        {
+            int result;
+            result = num1 * num2;
+            return result;
+        }
+
+        public int addNum(int num1, int num2)
+        {
+            int result;
+            result = num1 + num2;
+            return result;
+        }
+    }
+}
