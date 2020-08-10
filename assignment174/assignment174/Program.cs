1462533e5b3205005afbd6b19c7269c1f21c74b6
Chromatonics <Pallaslih6@gmail.com>
2020-08-10 12:53:05 -0600

    Added Assignment 174

---
+++ assignment174/assignment174/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace assignment174
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sample = new Employee();
+            sample.FirstName = "Sample";
+            sample.LastName = "Student";
+            sample.SayName();
+            Console.ReadLine();
+        }
+    }
+}
