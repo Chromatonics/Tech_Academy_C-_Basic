2bcf56f84d6e160eccf2716eb92ac7052465f26d
Chromatonics <Pallaslih6@gmail.com>
2020-08-10 13:50:28 -0600

    Added Abstract Assignment

---
+++ abstractAssign/abstractAssign/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace abstractAssign
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + LastName);
+        }
+    }
+}
