baf0762b632f6e07a765c14e5e38129a511e5a30
Chromatonics <Pallaslih6@gmail.com>
2020-08-11 14:21:52 -0600

    Assignment page# 187

---
+++ overloadEquals/overloadEquals/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloadEquals
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + LastName);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ overloadEquals/overloadEquals/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloadEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sample = new Employee();
+            sample.FirstName = "Sample";
+            sample.LastName = "Student";
+            sample.Id = 4;
+            sample.SayName();
+
+            Employee five = new Employee();
+            five.FirstName = "Bob";
+            five.LastName = "Boberson";
+            five.Id = 5;
+            five.SayName();
+
+            Console.WriteLine(sample.Id == five.Id);
+            Console.ReadLine();
+        }
+
+        //public static equalID(Person Id)
+        //{
+
+        //}
+    }
+}
