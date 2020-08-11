58e65a8c6f739c50966449ff1198114748d3ae01
Chromatonics <Pallaslih6@gmail.com>
2020-08-11 14:52:14 -0600

    generic Assign
    Assignment for Page #190

---
+++ genericAssign/genericAssign/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee printer = new Employee();
+
+            printer.Things<string>("Blue");
+            printer.Things("Yellow");
+            printer.Things("Purple");
+            printer.Things("Red");
+
+            printer.Things<int>(100);
+            printer.Things(200);
+            printer.Things(300);
+            printer.Things(400);
+            Console.ReadLine();
+        }
+    }
+}
