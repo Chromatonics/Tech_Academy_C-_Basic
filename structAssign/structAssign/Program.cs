023889d02ffbf5b354117f1b47dc11e33b33b7f7
Chromatonics <Pallaslih6@gmail.com>
2020-08-11 16:13:36 -0600

    structAssign
    Assignment for page #200

---
+++ structAssign/structAssign/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 12.4M;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
