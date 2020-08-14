7ad749c43ab46d218c975a1f155d99a525070eeb
Chromatonics <Pallaslih6@gmail.com>
2020-08-14 15:04:58 -0600

    Added constructor1
    Completed assignment for page#230

---
+++ constructor1/constructor1/Constructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor1
+{
+    public class Constructor
+    {
+         public int Day { get; set; }
+         public int Month { get; set; }
+         public int Year { get; set; }
+
+         public Constructor(int day) : this(day, 1) { }
+
+         public Constructor(int day, int month) : this(day, month, 1900) { }
+
+         public Constructor(int day, int month, int year)
+         {
+             this.Day = day;
+             this.Month = month;
+             this.Year = year;
+         }
+
+        public override string ToString()
+        {
+            return "Day: " + Day + "   Month: " + Month + "   Year: " + Year;
+        }
+
+    }
+    
+}
+
---
+++ constructor1/constructor1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int X = 0;
+
+            var variation = "This is a variable";
+
+            Constructor dayDay = new Constructor(1);
+            Console.WriteLine(dayDay);
+            Console.ReadLine();
+
+            Constructor dayMonth = new Constructor(3, 6);
+            Console.WriteLine(dayMonth);
+            Console.ReadLine();
+
+            Constructor daymonthY = new Constructor(23, 8, 2000);
+            Console.WriteLine(daymonthY);
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
