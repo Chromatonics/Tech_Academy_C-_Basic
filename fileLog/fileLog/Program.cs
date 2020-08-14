46076826e17fe5bf75258c67f1a009790d8b6451
Chromatonics <Pallaslih6@gmail.com>
2020-08-14 10:59:48 -0600

    Added fileLog
    Completed assignment for page #219

---
+++ fileLog/fileLog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number:");
+            string yNum = Console.ReadLine();
+            int tNum = Convert.ToInt32(yNum);
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Andrew Smith\Desktop\fileLogc#.txt", true))
+            {
+                file.WriteLine(yNum);
+            }
+
+            string result = "";
+            using (StreamReader sr = new StreamReader(@"C:\Users\Andrew Smith\Desktop\fileLogc#.txt"))
+            {
+                while ((result = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
