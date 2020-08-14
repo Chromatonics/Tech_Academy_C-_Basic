20d56eeb9940ba1d9415b5ab0dd7fde190c486a2
Chromatonics <Pallaslih6@gmail.com>
2020-08-14 14:14:54 -0600

    Added dateTime
    Completed assignment for page#222

---
+++ dateTime/dateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine(localDate);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number ");
+            string value1 = Console.ReadLine();
+            int value2 = Convert.ToInt32(value1);
+
+            DateTime dt2 = localDate.AddHours(value2);
+            Console.WriteLine(dt2);
+            Console.ReadLine();
+        }
+    }
+}
