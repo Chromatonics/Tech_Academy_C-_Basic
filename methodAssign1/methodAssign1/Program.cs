218e44b3e09a5d4596d227c2ea30079b06bb1ed1
Chromatonics <Pallaslih6@gmail.com>
2020-08-16 14:43:55 -0600

    methodAssign1
    Completed assignment for page#164

---
+++ methodAssign1/methodAssign1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssign1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triplet addition = new Triplet();
+            int sum1 = addition.Math(8, 17);
+            Console.WriteLine(sum1);
+
+            Triplet subtraction = new Triplet();
+            double sub1 = subtraction.Math(2.78, 2.08);
+            Console.WriteLine(sub1);
+
+            Triplet product = new Triplet();
+            int pro1 = product.Math("4");
+            Console.WriteLine(pro1);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodAssign1/methodAssign1/Triplet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssign1
+{
+    class Triplet
+    {
+        public int Math(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        public double Math(double a, double b)
+        {
+            double sub = a - b;
+            return sub;
+        }
+
+        public int Math(string c)
+        {
+            int d = Convert.ToInt32(c);
+            int pro = d * 3;
+            return pro;
+        }
+
+    }
+}
