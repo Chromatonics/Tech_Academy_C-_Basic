03a3f733b720453ae43fbc7e763626f2138b97b5
Chromatonics <Pallaslih6@gmail.com>
2020-08-16 15:50:18 -0600

    methodAssign2
    Completed assignment for page#165

---
+++ methodAssign2/methodAssign2/Optional.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssign2
+{
+    public class Optional
+    {
+        static public void scholar(int x, int y = 2)
+                                   
+        {
+            int z = x + y;
+            Console.WriteLine(z);
+        }
+    }
+}
---
+++ methodAssign2/methodAssign2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssign2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number");
+            int d1 = Convert.ToInt32(Console.ReadLine());
+
+            Optional.scholar(d1);
+
+            Console.WriteLine("Enter a second number if you wish");
+            int d2 = Convert.ToInt32(Console.ReadLine());
+
+            Optional.scholar(d1, d2);
+
+            Console.ReadLine();
+        }
+    }
+}
