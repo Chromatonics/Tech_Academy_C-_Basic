c8a4b39a238c05589e6b68da932704174bdec583
Chromatonics <Pallaslih6@gmail.com>
2020-08-16 16:21:15 -0600

    methodAssign3
    Completed assignment for page#166

---
+++ methodAssign3/methodAssign3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssign3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Void empty = new Void();
+
+            empty.New(5, 13);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ methodAssign3/methodAssign3/Void.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssign3
+{
+    class Void
+    {
+        public void New(int x, int y)
+        {
+            int z = x + 5;
+            Console.WriteLine(z);
+
+            Console.WriteLine(y);
+
+            Console.WriteLine("The sum of x and 5 is " + z);
+            Console.WriteLine("The value of y is " + y);
+        }
+    }
+}
