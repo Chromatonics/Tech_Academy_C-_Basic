1323d984260c131a2fbe557b9b41acb2182f3a06
Chromatonics <Pallaslih6@gmail.com>
2020-08-20 14:42:34 -0600

    lambda1
    Completed assignment for page#203

---
+++ lambda1/lambda1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda1
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return "ID: " + Id + "   First Name: " + FirstName+ "   Last Name: " +LastName;
+        }
+    }
+}
---
+++ lambda1/lambda1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> employee = new List<Person>();
+                employee.Add(new Person() { Id = 1, FirstName = "Joe", LastName = "Smith" });
+                employee.Add(new Person() { Id = 2, FirstName = "Jill", LastName = "Smith" });
+                employee.Add(new Person() { Id = 3, FirstName = "Jane", LastName = "Smith" });
+                employee.Add(new Person() { Id = 4, FirstName = "Josephine", LastName = "Smith" });
+                employee.Add(new Person() { Id = 5, FirstName = "Joe", LastName = "Johnson" });
+                employee.Add(new Person() { Id = 6, FirstName = "Jen", LastName = "Smith" });
+                employee.Add(new Person() { Id = 7, FirstName = "Jessie", LastName = "Smith" });
+                employee.Add(new Person() { Id = 8, FirstName = "Jim", LastName = "Smith" });
+                employee.Add(new Person() { Id = 9, FirstName = "John", LastName = "Smith" });
+                employee.Add(new Person() { Id = 10, FirstName = "Jorge", LastName = "Smith" });
+
+
+            List<Person> joes = new List<Person>();
+            foreach (Person f in employee)
+            {
+                if (f.FirstName == "Joe")
+                {
+                    joes.Add(f);
+                }
+            }
+            foreach (Person f in joes)
+            {
+                Console.WriteLine(f);
+            }
+            Console.ReadLine();
+
+
+            List<Person> newList = employee.Where(x => x.FirstName == "Joe").ToList();
+            foreach (Person g in newList)
+            {
+                Console.WriteLine(g);
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
