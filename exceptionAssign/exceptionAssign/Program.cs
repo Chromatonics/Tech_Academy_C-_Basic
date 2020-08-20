5e99fb438e1fcb6ae3a96a6020d6b89c9458ed78
Chromatonics <Pallaslih6@gmail.com>
2020-08-20 16:01:27 -0600

    exceptionAssign
    Completed assignment for page #234

---
+++ exceptionAssign/exceptionAssign/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionAssign
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What is your age?");
+                var age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                DateTime localDate = DateTime.Now;
+                int year = localDate.Year;
+
+                int span = year - age;
+                Console.WriteLine("The year you were born was: " + span);
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter a positive value greater than zero");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong");
+            }
+        }
+    }
+}
